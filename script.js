bc15bb5352938b23a6cdbf360c214d2bedeaa036
leonardo silva <silvaleonardo.ti@gmail.com>
2020-11-30 00:54:32 -0300

    Implementacao do comportamente de accordion no secao de FAQ

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li')
-const tabContent = document.querySelectorAll('.js-tabcontent section')
+function initTabNav(){
 
-if(tabMenu.length && tabContent.length){
+    const tabMenu = document.querySelectorAll('.js-tabmenu li')
+    const tabContent = document.querySelectorAll('.js-tabcontent section')
+
+    if(tabMenu.length && tabContent.length){
 
     activeTab(0)
     
@@ -10,10 +12,34 @@ if(tabMenu.length && tabContent.length){
         tabContent[index].classList.add('ativo')
     }
     
-    Array.from(tabMenu)
-        .map((item, index) => 
-            item.addEventListener('click', () => 
-                activeTab(index))
-            )
+    Array.from(tabMenu).map((item, index) => item.addEventListener('click', () => activeTab(index)))
 
 }
+
+}
+
+initTabNav()
+
+
+function initAccordion(){
+    const accordionList = document.querySelectorAll('.js-accordion dt')
+
+    if(accordionList.length){
+        accordionList[0].classList.add('ativo')
+        accordionList[0].nextElementSibling.classList.add('ativo')
+
+        function activeAccordion(){   
+            this.classList.toggle('ativo')
+            this.nextElementSibling.classList.toggle('ativo')
+        }
+
+        // Array.from(accordionList).map(item => item.addEventListener('click', () => activeAccordion))
+        accordionList.forEach((item) => {
+        item.addEventListener('click', activeAccordion)
+    })
+    }
+
+    
+}
+
+initAccordion()
