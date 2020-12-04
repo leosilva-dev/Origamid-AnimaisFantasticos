47134684d8441a1ecb535e0a61639322c28577e0
leonardo silva <silvaleonardo.ti@gmail.com>
2020-12-04 01:42:23 -0300

    Implementação JS que controle os eventos de clique fora de determinado componente. Utilizado no dropdown menu e futuramente no menu mobile

---
+++ js/modules/outsideclick.js
@@ -0,0 +1,22 @@
+export default function outsideClick(element, events, callback){
+    const html = document.documentElement
+    const outside = 'data-outside'
+
+    if(!element.hasAttribute(outside)){
+        events.forEach(e => {
+            html.addEventListener(e, handleOutsideClick)
+        })
+
+        element.setAttribute(outside, '')
+    }
+
+    function handleOutsideClick(event){
+        if(!element.contains(event.target)){
+            callback()
+            element.removeAttribute(outside)
+            events.forEach(e => {
+                html.removeEventListener(e, handleOutsideClick)
+            })
+        }
+    }
+}
