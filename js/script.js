b46dbfd712277e53bab019e60b7830e38b303d9a
leonardo silva <silvaleonardo.ti@gmail.com>
2020-12-18 01:02:06 -0300

    Carregando as informações de quantidades de animais através de um arquivo JSON usando o método fetch, simulando a chamado de uma api real.

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ import initDropDownMenu from './modules/dropdown-menu.js'
 import initMenuMobile from './modules/menu-mobile.js'
 import initAnimaNumeros from './modules/anima-numeros.js'
 import initFuncionamento from './modules/funcionamento.js'
+import initFetchAnimais from './modules/fetch-animais.js'
 
 
 initScrollSuave()
@@ -18,5 +19,6 @@ initModal()
 initTooltip()
 initDropDownMenu()
 initMenuMobile()
+initFetchAnimais()
 initAnimaNumeros()
 initFuncionamento()
